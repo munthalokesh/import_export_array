47d1abd14d9af48546d51e785875a7b24ba38311
muntha lokesh <munthalokesh96@gmail.com>
2021-08-01 17:04:33 +0000

    changed array name

--- exportArray.mjs
+++ exportArray.mjs
@@ -1,2 +1,2 @@
-const array = ["countries", 190, "continents", 7, false, 6.2];
-export default array;
+const example_array = ["countries", 190, "continents", 7, false, 6.2];
+export default example_array;
